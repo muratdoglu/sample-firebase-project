01c808926d14e1d3ce299b89612ad9de6740b665
muratdegirmencioglu <muratdegirmencioglu@teknasyon.com>
2020-10-22 17:55:20 +0300

    first  commit

--- app/src/main/java/com/pickup/rsbyoqg0ny/data/repository/MainRepository.kt
+++ app/src/main/java/com/pickup/rsbyoqg0ny/data/repository/MainRepository.kt
@@ -7,7 +7,7 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-
+//hi
 class MainRepository(val restInterface: RestInterface) {
     inline fun getPopulars(
         page: Int,
